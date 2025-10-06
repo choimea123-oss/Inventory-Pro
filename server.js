d0bba560a3178d097d6165c5a35e77b1d3f30469
AJ <choimea123@gmail.com>
2025-10-06 22:35:02 +0800

    Switch to mysql2 for MySQL 8 compatibility

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('dotenv').config(); // Load environment variables FIRST
 
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const helmet = require("helmet");
